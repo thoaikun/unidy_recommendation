1e6c4841a502c5a3859e17397e62d368f4c9a9df
Huy-Thai-3105 <80185943+Huy-Thai-3105@users.noreply.github.com>
2024-01-29 17:29:45 +0700

    fix index id

--- app/routes/recommend_routes.py
+++ app/routes/recommend_routes.py
@@ -13,6 +13,7 @@ def index():
     data = request.get_json()
     print(data)
     genome_tag = pd.read_csv('/Users/truonghuythai/Documents/unidy_project/unidy_recommendation/model/data/campaign_type.csv')
+    campaign = pd.read_csv('/Users/truonghuythai/Documents/unidy_project/unidy_recommendation/model/data/campaign.csv')
     matrix_score = np.array(genome_tag)
     # print(matrix_score)
     model = joblib.load('/Users/truonghuythai/Documents/unidy_project/unidy_recommendation/model/knnModel.joblib')
@@ -22,7 +23,9 @@ def index():
     # vector = matrix_score[index]
     # print(vector)
     distances, indices = model.kneighbors(vector)
-
-    index_activity = [int(item) for sublist in indices for item in sublist]
+    index_activity = [int(item) +1 for sublist in indices for item in sublist]
+    listIds = genome_tag.iloc[index_activity]['campaign_id']
+    campaign_ids_list = campaign[campaign['campaign_id'].isin(listIds)]['campaign_id'].tolist() 
 
     return json.dumps(index_activity)
+
